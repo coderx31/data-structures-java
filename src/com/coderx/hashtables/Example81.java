d6648340007c3abeccbb55d8c76dd9ab32ed2dc9
coderx31 <shenal.fernando10@gmail.com>
2021-06-22 16:14:33 +0530

    hashtable done

---
+++ src/com/coderx/hashtables/Example81.java
@@ -0,0 +1,38 @@
+package com.coderx.hashtables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example81 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("Tag","Day");
+        map.put("Hut","Hat");
+        map.put("Uhr", "Clock");
+
+        System.out.println(map);
+//        System.out.println(map.size());
+//        System.out.println(map.keySet());
+//        System.out.println(map.values());
+//        System.out.println(map.get("Tag"));
+
+        // loop through the hashmap
+
+        // keySet()
+//        for(Object key : map.keySet()){
+//            System.out.println("Key: "+key+" value: "+map.get(key));
+//        }
+
+        // values()
+//        for(Object value : map.values()){
+//            System.out.println("value: "+value);
+//        }
+
+        // entrySet()
+
+//        for(Object entry : map.entrySet()){
+//            System.out.println("Key: "+entry.);
+//        }
+
+    }
+}
